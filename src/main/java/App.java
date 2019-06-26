f3e8f784b8143d95c783c10521c2d9deb375c08c
vmuchui <kaigongimuchui@gmail.com>
2019-06-26 14:07:57 +0300

    added code to assign a port.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,8 +9,19 @@ import java.util.Map;
 
 
 public class App {
+        static int getHerokuAssignedPort() {
+            ProcessBuilder processBuilder = new ProcessBuilder();
+            if (processBuilder.environment().get("PORT") != null) {
+                return Integer.parseInt(processBuilder.environment().get("PORT"));
+            }
+            return 4567;
+        }
+
     public static void main(String[] args) {
+
+        port(getHerokuAssignedPort());
         staticFileLocation("/public");
+
         get("/", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
             return new ModelAndView(model, "index.hbs");
